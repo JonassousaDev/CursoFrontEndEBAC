ee3ea8c3eeb1d039bffa52e45922fd988baa6df5
jonassousaDev <jonassousa.dev@gmail.com>
2022-06-11 14:35:16 -0300

    modulo26-exercicio

--- javascript-main/src/exams.js
+++ javascript-main/src/exams.js
@@ -1,38 +1,38 @@
-import months from './months.js'
+// import months from './months.js'
+import imoveis from './imoveis.js'
 
-// Filtra meses
-const filteredMonths = months.filter( (months)=> {
-    return months.days === 31
+// Filtra imoveis
+const filteredImoveis = imoveis.filter( (imoveis)=> {
+    return imoveis.tipo === 'casa'
+})
+console.log(filteredImoveis)
+ let toPrint = ''
+
+filteredImoveis.forEach(tipo => {
+   toPrint = tipo.tipo
+
 })
 
-console.log(filteredMonths)
+// suites
 
-let toPrint = ''
-
-// Lista meses
-filteredMonths.forEach(month => {
-   toPrint += month.month + ', '
-});
-
-// for( let c = 0; c < filteredMonths.length; c++ ){
-
-// }
-
-let meses = [30,30,30] // array comum
-
-// Reduce - função com todos os itens do array e devolve um valor único
-let sumMonthDays = filteredMonths.reduce( (prev, next) => {
-    return {days: prev.days + next.days}
+let sumSuite = filteredImoveis.reduce( (prev, next) => {
+    return {suites: prev.suites + next.suites}
 })
 
-// Map - funções em arrays de retorno individual (cada valor array)
-let cachorros = [10, 15, 7, 8, 9]
 
-let years = cachorros.map( (cachorro => {
-    return cachorro * 7
-}))
+// soma dos comdominios
+let sumCondominios = filteredImoveis.reduce( (prev, next) => {
+    return {comdominio: prev.comdominio + next.comdominio}
+})
+
+// soma dos vagas
+let sumVagas = filteredImoveis.reduce( (prev, next) => {
+    return {vagas: prev.vagas + next.vagas}
+})
+
 
 document.getElementById('main').innerHTML = 
-    toPrint + '<br> Soma dos dias dos meses selecionados: ' 
-    + sumMonthDays.days + '<br> idade humana dos cachorros: '  
-    + years + ' '
+    toPrint + 
+    '<br> Soma dos comdominio dos imoveis selecionados: ' + 'R$' + sumCondominios.comdominio + 
+    '<br> Soma das suites selecionados: ' + sumSuite.suites + ' Suites ' +
+    '<br> Soma das vagas selecionados: ' + sumVagas.vagas + ' vagas '
